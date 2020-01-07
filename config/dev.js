64a4cf5360c56811c645ac2669a6c3ab45fcd828
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2020-01-07 11:45:04 +0000

    initial setup

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00-lie0b.mongodb.net:27017,cluster0-shard-00-01-lie0b.mongodb.net:27017,cluster0-shard-00-02-lie0b.mongodb.net:27017/advanced-node-starter?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
   cookieKey: '123123123'
 };
