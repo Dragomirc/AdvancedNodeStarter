5c42c72034aea685597c55559fc4393d40524d61
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2020-01-08 14:50:18 +0000

    refactores the cache service to register hsets against user id, thus just the required chache data can be deleted upon new blog post creation

--- services/cache.js
+++ services/cache.js
@@ -3,21 +3,36 @@ const redis = require('redis');
 const util = require('util');
 const redisUri = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUri);
-client.get = util.promisify(client.get);
-client.set = util.promisify(client.set);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+  return this;
+};
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
   const key = JSON.stringify({
     ...this.getQuery(),
     collection: this.mongooseCollection.name
   });
-  const cachedResult = await client.get(key);
-
+  const cachedResult = await client.hget(this.hashKey, key);
   if (cachedResult) {
-    console.log('redis');
-    return Prmomise.resolve(cachedResult);
+    const doc = JSON.parse(cachedResult);
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
   }
-  console.log('Query');
-  return exec.apply(this, arguments);
+  const queryResult = await exec.apply(this, arguments);
+  client.hset(this.hashKey, key, JSON.stringify(queryResult));
+  return queryResult;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 };
