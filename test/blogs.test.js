d877287935cc58af89053f4b46fec93b1d8dec62
Dragomir <svedava@mail.ru>
2020-01-12 23:31:10 +0000

    added test for creating the blog post

--- test/blogs.test.js
+++ test/blogs.test.js
@@ -13,19 +13,40 @@ describe('When logged in', () => {
 		await superPage.login();
 		await superPage.click('a.btn-floating');
 	});
-	test.only('in can see blog create form', async () => {
+	test('in can see blog create form', async () => {
 		const text = await superPage.getContentsOf('form label');
 		expect(text).toEqual('Blog Title');
 	});
+	describe('And using valid inputs', () => {
+		const titleText = 'My Title';
+		const contentText = 'My Content';
+		beforeEach(async () => {
+			await superPage.type('input[name="title"]', titleText);
+			await superPage.type('input[name="content"]', contentText);
+			await superPage.click('form button');
+		});
+		test('takes user to review screen', async () => {
+			const text = await superPage.getContentsOf('form h5');
+			expect(text).toEqual('Please confirm your entries');
+		});
+		test('submitting then saving adds blog to index page', async () => {
+			await superPage.click('button.green');
+			await superPage.waitFor('div.card');
+			const title = await superPage.getContentsOf('.card-title');
+			const content = await superPage.getContentsOf('p');
+			expect(title).toEqual(titleText);
+			expect(content).toEqual(contentText);
+		});
+	});
 	describe('And using invalid inputs', () => {
 		beforeEach(async () => {
-			superPage.click('form button');
+			await superPage.click('form button');
 		});
 		test('the form shows an error message', async () => {
-			const titleError = superPage.getContentsOf('.title .red-text');
+			const titleError = await superPage.getContentsOf('.title .red-text');
+			const contentError = await superPage.getContentsOf('.content .red-text');
 			expect(titleError).toEqual('You must provide a value');
+			expect(contentError).toEqual('You must provide a value');
 		});
-		const contentError = superPage.getContentsOf('.content .red-text');
-		expect(contentError).toEqual('You must provide a value');
 	});
 });
